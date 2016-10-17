6155e99171bad1f7bd10b2a7564dd3bdb2f89197
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-10-17 21:53:00 +0900

    Add minimum editor component

---
+++ src/js/components/Editor.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Editor extends Component {
+  render() {
+    return (
+      <div>
+        hello Editor Component!!!!
+      </div>
+    );
+  }
+}
+
+export default Editor;
